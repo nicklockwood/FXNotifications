895e7501d2d54e26afc0b37f0eda40c775e60808
James Clarke <james@plex.tv>
2014-03-12 08:33:48 -0700

    Recreated the iOS example app for OS X.

---
+++ Examples/NotificationsTest/OS X/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NotificationsTest
+//
+//  Created by James Clarke on 3/11/14.
+//  Copyright (c) 2014 Caffeine and Cocoa. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Examples/NotificationsTest/OS X/WindowController.h
@@ -0,0 +1,13 @@
+//
+//  WindowController.h
+//  NotificationsTest
+//
+//  Created by James Clarke on 3/11/14.
+//  Copyright (c) 2014 Caffeine and Cocoa. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface WindowController : NSWindowController
+
+@end
